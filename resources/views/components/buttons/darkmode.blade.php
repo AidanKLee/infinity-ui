03bc9022a81b60c881cb0c25fbd871083848e73b
aidan <aidan@wecreatedigital.co.uk>
2024-06-23 02:34:33 +0100

    Update dark mode button styling.

--- resources/views/components/buttons/darkmode.blade.php
+++ resources/views/components/buttons/darkmode.blade.php
@@ -1,3 +1,3 @@
-<button @attributes(null, null, 'p-3.5 bg-gradient-to-br from-dark/2.5 to-dark/10 rounded-md duration-200 focus-none hover:bg-dark/2.5 focus:bg-dark/2.5 focus:ring-2 focus:ring-primary dark:from-light/2.5 dark:to-light/10 dark:hover:bg-light/2.5 dark:focus:bg-light/2.5 dark:focus:ring-primary-dark')>
+<button @attributes(null, null, 'p-3.5 bg-gradient-to-br from-dark/2.5 to-dark/10 rounded-md duration-200 focus-none hover:text-primary hover:bg-dark/2.5 focus:text-primary focus:bg-dark/2.5 focus:ring-2 focus:ring-primary dark:from-light/2.5 dark:to-light/10 dark:hover:bg-light/2.5 dark:hover:text-primary-dark dark:focus:bg-light/2.5 dark:focus:ring-primary-dark dark:focus:text-primary-dark')>
     <x-icons.moon class="w-4 h-4" />
 </button>
