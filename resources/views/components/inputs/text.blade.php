bd3ce5f9f01c6215d163aad949e85451828e0b3d
aidan <aidan@wecreatedigital.co.uk>
2024-06-26 00:31:32 +0100

    Minor updates to the text input.

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -8,11 +8,11 @@
         <label @attributes(['id' => 'for'], null, [
             'block w-fit mb-1 opacity-70',
             'absolute pointer-events-none pt-3.5 ml-2 duration-200 peer-disabled:opacity-20 group-focus-within:pt-1 group-focus-within:opacity-50 group-focus-within:text-xs peer-[:not(:placeholder-shown)]:pt-1 peer-[:not(:placeholder-shown)]:opacity-50 peer-[:not(:placeholder-shown)]:text-xs' => isset($floating),
-            'order-1' => !isset($floating),
+            'text-sm order-1' => !isset($floating),
         ])>{{ $label }}</label>
     @endisset
     @isset($maxlength)
-        <div class="text-2xs text-right mt-0.5" :class="{ 'text-danger': text.length > maxlength }" x-text="`${text.length} / ${maxlength}`"></div>
+        <div @class(["text-2xs text-right mt-0.5", 'order-3' => !isset($floating)]) :class="{ 'text-danger': text.length > maxlength }" x-text="`${text.length} / ${maxlength}`"></div>
     @endisset
 </div>
 
